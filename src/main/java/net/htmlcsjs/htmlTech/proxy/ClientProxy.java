dc7d354988774283f730165d8b8d0a451d3ff2c5
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2021-09-20 18:17:11 +0100

    Added a cape for my minecraft account when the mod is loaded

--- src/main/java/net/htmlcsjs/htmlTech/proxy/ClientProxy.java
+++ src/main/java/net/htmlcsjs/htmlTech/proxy/ClientProxy.java
@@ -1,9 +1,19 @@
 package net.htmlcsjs.htmlTech.proxy;
 
+import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import net.htmlcsjs.htmlTech.api.HTTextures;
+import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.network.NetworkPlayerInfo;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Map;
+import java.util.UUID;
 
 @SideOnly(Side.CLIENT)
 @Mod.EventBusSubscriber(Side.CLIENT)
@@ -13,5 +23,16 @@ public class ClientProxy extends CommonProxy {
     public void preLoad() {
         super.preLoad();
         HTTextures.preInit();
+    }
+
+    @SubscribeEvent
+    public static void onPlayerRender(RenderPlayerEvent.Pre event) {
+        AbstractClientPlayer clientPlayer = (AbstractClientPlayer) event.getEntityPlayer();
+        UUID capedUUID = UUID.fromString("5d7073e3-882f-4c4a-94b3-0e5ba1c11e02");
+        if (capedUUID.equals(clientPlayer.getUniqueID()) && clientPlayer.hasPlayerInfo() && clientPlayer.getLocationCape() == null) {
+            NetworkPlayerInfo playerInfo = ObfuscationReflectionHelper.getPrivateValue(AbstractClientPlayer.class, clientPlayer, 0);
+            Map<MinecraftProfileTexture.Type, ResourceLocation> playerTextures = ObfuscationReflectionHelper.getPrivateValue(NetworkPlayerInfo.class, playerInfo, 1);
+            playerTextures.put(MinecraftProfileTexture.Type.CAPE, HTTextures.HTMLTECH_CAPE);
+        }
     }
 }
