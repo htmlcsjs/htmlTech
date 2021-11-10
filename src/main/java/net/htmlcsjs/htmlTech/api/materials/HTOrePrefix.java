737aac466b09c0f12aa7073b61d87f17693e8dba
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2021-11-10 17:14:09 +0000

    amendment for a small "bug" i found

--- src/main/java/net/htmlcsjs/htmlTech/api/materials/HTOrePrefix.java
+++ src/main/java/net/htmlcsjs/htmlTech/api/materials/HTOrePrefix.java
@@ -1,9 +1,14 @@
 package net.htmlcsjs.htmlTech.api.materials;
 
+import gregtech.api.GTValues;
 import gregtech.api.unification.material.info.MaterialIconType;
 import gregtech.api.unification.ore.OrePrefix;
+import gregtech.api.util.GTUtility;
 import gregtech.common.items.MetaItems;
-import net.htmlcsjs.htmlTech.htmlTech;
+import net.minecraft.client.resources.I18n;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static gregtech.api.unification.ore.OrePrefix.Flags.ENABLE_UNIFICATION;
 import static net.htmlcsjs.htmlTech.api.materials.HTMaterials.LASER;
@@ -20,7 +25,16 @@ public class HTOrePrefix {
     private static void initLasers() {
         laserIcon = new MaterialIconType("laser");
 
-        laser = new OrePrefix("laser", -1, null, laserIcon, ENABLE_UNIFICATION, material -> material.hasProperty(LASER));
+        laser = new OrePrefix("laser", -1, null, laserIcon, ENABLE_UNIFICATION, material -> material.hasProperty(LASER),  material -> {
+            List<String> textList = new ArrayList<>();
+
+            int voltage = material.getProperty(LASER).voltage;
+            int amperage = material.getProperty(LASER).amperage;
+
+            textList.add(I18n.format("htmltech.laser.voltage", voltage, GTValues.VN[(GTUtility.getTierByVoltage(voltage))]));
+            textList.add(I18n.format("htmltech.laser.amperage", amperage));
+            return textList;
+        });
 
         MetaItems.addOrePrefix(laser);
     }
