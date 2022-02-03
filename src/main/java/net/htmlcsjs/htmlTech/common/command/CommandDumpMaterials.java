2e3d17b05c3aa767d0bc8184aaa86358c4cdafd1
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2022-02-03 20:32:42 +0000

    fix npe

--- src/main/java/net/htmlcsjs/htmlTech/common/command/CommandDumpMaterials.java
+++ src/main/java/net/htmlcsjs/htmlTech/common/command/CommandDumpMaterials.java
@@ -34,64 +34,64 @@ public class CommandDumpMaterials extends CommandTreeBase {
         Map<String, Object> materialsData = new HashMap<>();
         List<Map<String, Object>> materialsArray = new ArrayList<>();
         for (Material material: GregTechAPI.MaterialRegistry.getAllMaterials()) {
+            Map<String, Object> materialData = new HashMap<>();
+            // dump simple stuff
+            materialData.put("unlocalized_name", material.getUnlocalizedName());
+            materialData.put("id", material.getId());
+            materialData.put("colour", material.getMaterialRGB());
+            materialData.put("blast_temp", material.getBlastTemperature());
+
+            // material properties stuff
+            Map<String, Object> propertiesMap = new HashMap<>();
+            MaterialProperties properties = material.getProperties();
+
+            if (properties.hasProperty(PropertyKey.WIRE)) {
+                WireProperties wireProperties = material.getProperty(PropertyKey.WIRE);
+                propertiesMap.put("wire", String.format("Voltage: %s, Amperage: %d, Loss: %d", GTUtility.getTierByVoltage(wireProperties.getVoltage()), wireProperties.getAmperage(), wireProperties.getLossPerBlock()));
+            }
+            if (properties.hasProperty(PropertyKey.TOOL)) {
+                ToolProperty toolProperty = material.getProperty(PropertyKey.TOOL);
+                propertiesMap.put("tool", String.format("Speed: %f, Durability: %d, Dmg: %f", toolProperty.getToolSpeed(), toolProperty.getToolDurability(), toolProperty.getToolAttackDamage()));
+            }
+            if (properties.hasProperty(PropertyKey.ORE)) {
+                propertiesMap.put("ore", true);
+            } else {
+                propertiesMap.put("ore", false);
+            }
             try {
-                Map<String, Object> materialData = new HashMap<>();
-                // dump simple stuff
-                materialData.put("unlocalized_name", material.getUnlocalizedName());
-                materialData.put("id", material.getId());
-                materialData.put("colour", material.getMaterialRGB());
-                materialData.put("blast_temp", material.getBlastTemperature());
-
-                // material properties stuff
-                Map<String, Object> propertiesMap = new HashMap<>();
-                MaterialProperties properties = material.getProperties();
-
-                if (properties.hasProperty(PropertyKey.WIRE)) {
-                    WireProperties wireProperties = material.getProperty(PropertyKey.WIRE);
-                    propertiesMap.put("wire", String.format("Voltage: %s, Amperage: %d, Loss: %d", GTUtility.getTierByVoltage(wireProperties.getVoltage()), wireProperties.getAmperage(), wireProperties.getLossPerBlock()));
-                }
-                if (properties.hasProperty(PropertyKey.TOOL)) {
-                    ToolProperty toolProperty = material.getProperty(PropertyKey.TOOL);
-                    propertiesMap.put("tool", String.format("Speed: %f, Durability: %d, Dmg: %f", toolProperty.getToolSpeed(), toolProperty.getToolDurability(), toolProperty.getToolAttackDamage()));
-                }
-                if (properties.hasProperty(PropertyKey.ORE)) {
-                    propertiesMap.put("ore", true);
-                } else {
-                    propertiesMap.put("ore", false);
-                }
                 if (properties.hasProperty(PropertyKey.BLAST)) {
                     BlastProperty blastProperty = material.getProperty(PropertyKey.BLAST);
                     propertiesMap.put("blast", String.format("Temp: %d, Gas Tier: %s", blastProperty.getBlastTemperature(), blastProperty.getGasTier().toString()));
                 }
-                if (properties.hasProperty(PropertyKey.FLUID_PIPE)) {
-                    FluidPipeProperties fluidPipeProperties = properties.getProperty(PropertyKey.FLUID_PIPE);
-                    propertiesMap.put("blast", String.format("Throughput: %d, Max Temp: %dK, Channels: %d", fluidPipeProperties.getThroughput(), fluidPipeProperties.getMaxFluidTemperature(), fluidPipeProperties.getTanks()));
-                }
-                if (properties.hasProperty(PropertyKey.ITEM_PIPE)) {
-                    ItemPipeProperties itemPipeProperties = properties.getProperty(PropertyKey.ITEM_PIPE);
-                    propertiesMap.put("blast", String.format("Throughput: %f, Priority: %d", itemPipeProperties.getTransferRate() * 64, itemPipeProperties.getPriority()));
-                }
-                if (properties.hasProperty(PropertyKey.FLUID)) {
-                    FluidProperty fluidProperty = properties.getProperty(PropertyKey.FLUID);
-                    propertiesMap.put("fluid_temp", String.format("Fluid Temp: %dK", fluidProperty.getFluidTemperature()));
-                }
-                if (properties.hasProperty(PropertyKey.PLASMA)) {
-                    PlasmaProperty plasmaProperty = properties.getProperty(PropertyKey.PLASMA);
-                    propertiesMap.put("plasma_temp", String.format("Plasma Temp: %dK", plasmaProperty.getPlasma().getTemperature()));
-                }
-
-                materialData.put("properties", propertiesMap);
-                // Component dumping
-                List<Map<String, Object>> componentsList = new ArrayList<>();
-                for (MaterialStack component : material.getMaterialComponents()) {
-                    Map<String, Object> componentData = new HashMap<>();
-                    componentData.put("name", component.material.getUnlocalizedName());
-                    componentData.put("amount", component.amount);
-                    componentsList.add(componentData);
-                }
-                materialData.put("components", componentsList);
-                materialsArray.add(materialData);
             } catch (Exception ignored) {}
+            if (properties.hasProperty(PropertyKey.FLUID_PIPE)) {
+                FluidPipeProperties fluidPipeProperties = properties.getProperty(PropertyKey.FLUID_PIPE);
+                propertiesMap.put("blast", String.format("Throughput: %d, Max Temp: %dK, Channels: %d", fluidPipeProperties.getThroughput(), fluidPipeProperties.getMaxFluidTemperature(), fluidPipeProperties.getTanks()));
+            }
+            if (properties.hasProperty(PropertyKey.ITEM_PIPE)) {
+                ItemPipeProperties itemPipeProperties = properties.getProperty(PropertyKey.ITEM_PIPE);
+                propertiesMap.put("blast", String.format("Throughput: %f, Priority: %d", itemPipeProperties.getTransferRate() * 64, itemPipeProperties.getPriority()));
+            }
+            if (properties.hasProperty(PropertyKey.FLUID)) {
+                FluidProperty fluidProperty = properties.getProperty(PropertyKey.FLUID);
+                propertiesMap.put("fluid_temp", String.format("Fluid Temp: %dK", fluidProperty.getFluidTemperature()));
+            }
+            if (properties.hasProperty(PropertyKey.PLASMA)) {
+                PlasmaProperty plasmaProperty = properties.getProperty(PropertyKey.PLASMA);
+                propertiesMap.put("plasma_temp", String.format("Plasma Temp: %dK", plasmaProperty.getPlasma().getTemperature()));
+            }
+
+            materialData.put("properties", propertiesMap);
+            // Component dumping
+            List<Map<String, Object>> componentsList = new ArrayList<>();
+            for (MaterialStack component : material.getMaterialComponents()) {
+                Map<String, Object> componentData = new HashMap<>();
+                componentData.put("name", component.material.getUnlocalizedName());
+                componentData.put("amount", component.amount);
+                componentsList.add(componentData);
+            }
+            materialData.put("components", componentsList);
+            materialsArray.add(materialData);
         }
         materialsData.put("materials", materialsArray);
         try {
