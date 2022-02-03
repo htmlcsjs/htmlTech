1956b33b5175c744275d90a5b3dcdb701a4ca319
htmlcsjs <46023024+htmlcsjs@users.noreply.github.com>
2022-02-03 17:43:40 +0000

    unsuccessful messing with laser rendering

--- src/main/java/net/htmlcsjs/htmlTech/common/item/behaviors/LaserGunBehaviour.java
+++ src/main/java/net/htmlcsjs/htmlTech/common/item/behaviors/LaserGunBehaviour.java
@@ -7,18 +7,18 @@ import gregtech.client.particle.GTLaserBeamParticle;
 import gregtech.client.particle.GTParticleManager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.*;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 
 public class LaserGunBehaviour implements IItemBehaviour {
-
     @Override
-    public ActionResult<ItemStack> onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        GTLaserBeamParticle particle = new GTLaserBeamParticle(world, Vector3.fromEntityCenter(player), Vector3.fromBlockPos(player.rayTrace(10, 5).getBlockPos()))
+    public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+        GTLaserBeamParticle particle = new GTLaserBeamParticle(world, Vector3.fromEntity(player), new Vector3(player.getLookVec()).add(0,-0.5,0).multiply(0,-1,0).multiply(100.1))
                 .setBody(new ResourceLocation(GTValues.MODID,"textures/fx/laser/laser.png")); // create a beam particle and set its texture.
         GTParticleManager.INSTANCE.addEffect(particle); // add it to the particle manager.
-
         ItemStack itemStack = player.getHeldItem(hand);
         return ActionResult.newResult(EnumActionResult.SUCCESS, itemStack);
     }
